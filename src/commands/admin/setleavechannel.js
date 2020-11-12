0b381791fb2083c681bb3d2677651a8a9fe682cf
GalackQSM <44106790+GalackQSM@users.noreply.github.com>
2020-11-12 01:21:01 +0000

    Update setleavechannel.js

--- src/commands/admin/setleavechannel.js
+++ src/commands/admin/setleavechannel.js
@@ -18,12 +18,12 @@ const { MessageEmbed } = require('discord.js');
 const { success } = require('../../utils/emojis.json');
 const { oneLine, stripIndent } = require('common-tags');
 
-module.exports = class SetFarewellChannelCommand extends Command {
+module.exports = class SetLeaveChannelCommand extends Command {
   constructor(client) {
     super(client, {
       name: 'setleavechannel',
       aliases: ['setfc', 'sfc'],
-      usage: 'setfarewellchannel <#salon/ID>',
+      usage: 'setleavechannel <#salon/ID>',
       description: oneLine`
         Définit le salon de texte du message d'adieu pour votre serveur.
         Ne fournissez aucun salon pour effacer le \`salon d'adieu\` actuel.
@@ -35,47 +35,46 @@ module.exports = class SetFarewellChannelCommand extends Command {
     });
   }
   run(message, args) {
-    let { farewell_channel_id: farewellChannelId, farewell_message: farewellMessage } = 
-      message.client.db.settings.selectFarewells.get(message.guild.id);
-    const oldFarewellChannel = message.guild.channels.cache.get(farewellChannelId) || '`Aucun`';
+    let { leave_channel_id: leaveChannelId, leave_message: leaveMessage } = 
+      message.client.db.settings.selectLeaves.get(message.guild.id);
+    const oldLeaveChannel = message.guild.channels.cache.get(leaveChannelId) || '`Aucun`';
+    const oldStatus = message.client.utils.getStatus(leaveChannelId, leaveMessage);
 
-    const oldStatus = message.client.utils.getStatus(farewellChannelId, farewellMessage);
-
-    if (farewellMessage && farewellMessage.length > 1024) farewellMessage = farewellMessage.slice(0, 1021) + '...';
+    if (leaveMessage && leaveMessage.length > 1024) leaveMessage = leaveMessage.slice(0, 1021) + '...';
     
     const embed = new MessageEmbed()
       .setTitle('Paramètres: `Salon d\'au revoir`')
       .setDescription(`Le \`Salon d\'au revoir\` a été mis à jour avec succès. ${success}`)
-      .addField('Message', message.client.utils.replaceKeywords(farewellMessage) || '`Aucun`')
+      .addField('Message', message.client.utils.replaceKeywords(leaveMessage) || '`Aucun`')
       .setThumbnail(message.guild.iconURL({ dynamic: true }))
       .setFooter("© 2020 - Alcatraz | Projet open-source")
       .setTimestamp()
       .setColor("#2f3136");
 
     if (args.length === 0) {
-      message.client.db.settings.updateFarewellChannelId.run(null, message.guild.id);
+      message.client.db.settings.updateLeaveChannelId.run(null, message.guild.id);
 
       const status = 'désactivé';
       const statusUpdate = (oldStatus != status) ? `\`${oldStatus}\` ➔ \`${status}\`` : `\`${oldStatus}\``; 
       
       return message.channel.send(embed
-        .spliceFields(0, 0, { name: 'Salon', value: `${oldFarewellChannel} ➔ \`Aucun\``, inline: true })
+        .spliceFields(0, 0, { name: 'Salon', value: `${oldLeaveChannel} ➔ \`Aucun\``, inline: true })
         .spliceFields(1, 0, { name: 'Statut', value: statusUpdate, inline: true })
       );
     }
 
-    const farewellChannel = this.getChannelFromMention(message, args[0]) || message.guild.channels.cache.get(args[0]);
-    if (!farewellChannel || (farewellChannel.type != 'text' && farewellChannel.type != 'news') || !farewellChannel.viewable) 
+    const leaveChannel = this.getChannelFromMention(message, args[0]) || message.guild.channels.cache.get(args[0]);
+    if (!leaveChannel || (leaveChannel.type != 'text' && leaveChannel.type != 'news') || !leaveChannel.viewable) 
       return this.sendErrorMessage(message, 0, stripIndent`
         Veuillez mentionner un texte ou un salon d'annonce accessible ou fournir un identifiant de salon de texte ou d'annonce valide
       `);
 
-    const status =  message.client.utils.getStatus(farewellChannel, farewellMessage);
+    const status =  message.client.utils.getStatus(leaveChannel, leaveMessage);
     const statusUpdate = (oldStatus != status) ? `\`${oldStatus}\` ➔ \`${status}\`` : `\`${oldStatus}\``;
 
-    message.client.db.settings.updateFarewellChannelId.run(farewellChannel.id, message.guild.id);
+    message.client.db.settings.updateLeaveChannelId.run(leaveChannel.id, message.guild.id);
     message.channel.send(embed
-      .spliceFields(0, 0, { name: 'Salon', value: `${oldFarewellChannel} ➔ ${farewellChannel}`, inline: true})
+      .spliceFields(0, 0, { name: 'Salon', value: `${oldLeaveChannel} ➔ ${leaveChannel}`, inline: true})
       .spliceFields(1, 0, { name: 'Statut', value: statusUpdate, inline: true})
     );
   }
